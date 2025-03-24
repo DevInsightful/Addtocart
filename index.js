0d88ba00205fff01072e8919928fa8f651454b0e
Zaid <syedzaidahmed21@gmail.com>
2025-03-25 02:13:58 +0500

    add service worker registration to enhance offline capabilities

--- index.js
+++ index.js
@@ -53,3 +53,18 @@ addButton.addEventListener("click", () => {
     push(itemsInDB, item);
   }
 });
+
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
