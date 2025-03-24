a45b4ba4f8a7a00bfdf61f7ad2d13de3e43db25a
Zaid <syedzaidahmed21@gmail.com>
2025-03-25 02:14:07 +0500

    add service worker to cache assets and enable offline support

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "addtocart-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/index.css",
+  "/index.js",
+  "/apple-touch-icon.png",
+  "/favicon-32x32.png",
+  "/favicon-16x16.png",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+];
+
+// Install event: cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event: serve cached files when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
